a6943c1fc3c6fa2f0e03dde91914c32b7e0b8f52
MarcBoreu <marcboreu@gmail.com>
2020-11-18 18:28:03 +0100

    Upgraded CSS Styles and include custom footer

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="btn btn-outline-light square" onClick={props.onClick}>
       {props.value}
     </button>
   );
 }
 
 class Board extends React.Component {
+  
   renderSquare(i) {
+    
     return (
       <Square
+        className="square-component"
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -24,7 +27,7 @@ class Board extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="col-xs-12 col-md-9 board-container-render">
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -96,7 +99,7 @@ class Game extends React.Component {
         'Go to game start';
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button className="btn btn-info btn-moves" onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
     });
@@ -108,26 +111,36 @@ class Game extends React.Component {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
 
+   
+
     return (
       <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={i => this.handleClick(i)}
-          />
+        <div className="title-container">
+          <h1 className="title">Tic Tac Toe</h1>
         </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+        <div className="col-12 game-board">
+            <div className="col-xs-12 status">
+              <h3>{status}</h3>
+            </div>
+          <div className="row justify-content-center">
+            <div className="col-xs-12 col-md-9 board-container">
+              <Board
+              squares={current.squares}
+              onClick={i => this.handleClick(i)}
+            />
+            </div>
+            <div className="col-xs-12 col-md-3 game-moves">
+              <ol>{moves}</ol>
+            </div>
+          </div>
         </div>
+        <FooterMarcBoreu/>
       </div>
     );
   }
 }
 
 // ========================================
-
-ReactDOM.render(<Game />, document.getElementById("root"));
 
 function calculateWinner(squares) {
   const lines = [
@@ -148,3 +161,90 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+
+
+
+
+
+
+
+
+const FooterMarcBoreu = () => {
+  
+  const date = new Date().getFullYear();
+
+  return (
+        <div className="site-footer">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 col-md-9">
+                <h6>About this</h6>
+                <a href="https://github.com/marcboreu?tab=repositories">
+                <button className="btn btn-outline-danger btn-code">View Code</button>
+                </a>
+                <p className="text-about">
+                # Tic-Tac-Toe Game with Create React App
+                <br/>
+                Game tic-tac-toe vitaminated
+                <br/>
+                ### 1.1. First Commit with orginal code
+                <br/>
+                Original code after first practise.
+                All made in only one index.js file. No components & hooks estructure.
+                <br/>
+                ### 1.2. New Skills
+                <br/>
+                1. Display the location for each move in the format (col, row) in the move history list.
+                <br/>
+                2. Bold the currently selected item in the move list.
+                <br/>
+                3. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+                <br/>
+                4. Add a toggle button that lets you sort the moves in either ascending or descending order.
+                <br/>
+                5. When someone wins, highlight the three squares that caused the win.
+                <br/>
+                6. When no one wins, display a message about the result being a draw.
+                <br/>
+                </p>
+              </div>
+              <div className="col-xs-12 col-md-3">
+                <h6>Quick Links</h6>
+                <ul className="footer-links">
+                  <li><a href="https://marcboreu.github.io/personal/">About me</a></li>
+                  <li><a href="https://marcboreu.github.io/personal/">Contact me</a></li>
+                  <li><a href="https://www.linkedin.com/in/marcboreu/">Linkedin</a></li>
+                  <li><a href="https://marcboreu.github.io/personal/portfolio">Portfolio</a></li>
+                  <li><a href="https://github.com/marcboreu?tab=repositories">Repositories</a></li>
+                </ul>
+              </div>
+            </div>
+            <hr/>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-sm-6 col-xs-12">
+                <p className="copyright-text">
+                  Copyright &copy; {date} All Rights Reserved || Created and developed by &nbsp;
+                  <a href="https://marcboreu.com" target="_blank" rel="noreferrer">
+                   Marc Boreu 
+                  <img className="marcboreu" src="https://raw.githubusercontent.com/marcboreu/marcboreu/master/logo-footer-mb.png" alt="Marc Boreu" target="_blank"/>
+                  </a>
+                </p>
+              </div>
+              <div className="col-md-4 col-sm-6 col-xs-12">
+                <ul className="social-icons">
+                  <li><a className="github" href="https://github.com/marcboreu" target="_blank" rel="noreferrer"><i className="fa fa-github"></i></a></li>
+                  <li><a className="linkedin" href="https://www.linkedin.com/in/marcboreu/" target="_blank" rel="noreferrer"><i className="fa fa-linkedin"></i></a></li>   
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+  );
+};
+
+
+
+ReactDOM.render(<Game />, document.getElementById("root"));
