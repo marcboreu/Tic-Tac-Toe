fd0b7603378e122021f9133665ca1b45dfc88606
MarcBoreu <marcboreu@gmail.com>
2020-11-18 20:14:28 +0100

    Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/index.js
+++ src/index.js
@@ -26,23 +26,18 @@ class Board extends React.Component {
   }
 
   render() {
+    let squares = [];
+    for (let i = 0; i < 3; i++) {
+      let rows = [];
+      for (let j= 0; j < 3; j++) {
+        rows.push(this.renderSquare(i * 3 + j));
+      }
+      squares.push(<div className="board-row" key={i}>{rows}</div>)
+    }
+
     return (
       <div className="col-xs-12 col-md-9 board-container-render">
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {squares}
       </div>
     );
   }
